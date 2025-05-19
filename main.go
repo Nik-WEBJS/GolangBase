a5db4791725255c12074c3545211a62e2b46605c
Nikita Fokin <nikita.fokin0123@gmail.com>
2025-05-20 00:50:12 +0500

    Transaction

--- main.go
+++ main.go
@@ -1,72 +1,36 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"math"
-)
+import "fmt"
 
-func main() { 
+func  main()  { 
+	transactions := []float64{}
 	for{
-		fmt.Println("Калькулятор индекса массы тела")
-		userHeight, userKg := getUserInput()
-		IMT, err := calculateIMT(userHeight, userKg)
-		if err != nil {
-			fmt.Println("Ошибка, неверно указан вес или рост")
-			continue
-		}
-		outputResult(IMT)
-		isRepeatCalculation := checkRepeatCalculation()
-		if !isRepeatCalculation {
+		transaction := scanTransaction()
+		if transaction == 0 {
 			break
 		}
+		transactions= append(transactions, transaction)		
 	}
+
+	balance := calculateBalance(transactions)
+
+	fmt.Printf("Ваш баланс: %.2f\n", balance)
+	
 }
 
-func outputResult(IMT float64) {
-	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", IMT)
-	fmt.Println(result)
-		switch {
-	case IMT < 16:
-		fmt.Println("У Вас сильный дефицит массы тела")
-	case IMT < 18.5:
-		fmt.Println("У Вас дефицит массы тела")
-	case IMT < 25:
-		fmt.Println("У Вас нормальная масса тела")
-	case IMT < 30:
-		fmt.Println("У Вас избыточная масса тела")
-	case IMT < 35:
-		fmt.Println("У Вас ожирение первой степени")
-	case  IMT < 40:
-		fmt.Println("У Вас ожирение второй степени")
-	default: fmt.Println("У Вас ожирение третьей степени")
-	}
+func scanTransaction() float64 {
+	var transaction float64
+	fmt.Println("Введите транзакцию: ")
+	fmt.Scan(&transaction)
+	return transaction
 }
 
-func calculateIMT(userHeight, userKg float64) (float64, error) {
-	const IMTPower = 2
-	if userKg <= 0 || userHeight <=0 {
-		return 0, errors.New("ERROR")
+func calculateBalance(transactions []float64) float64 {
+	sum := 0.0
+	
+	for _, value := range transactions {
+		sum += value
 	}
-	IMT := userKg / math.Pow(userHeight / 100, IMTPower)
-	return IMT, nil
-}
 
-func getUserInput() (float64, float64) {
-	var userHeight, userKg float64
-	fmt.Print("Введите ваш рост в сантиметрах: ")
-	fmt.Scan(&userHeight)
-	fmt.Print("Введите ваш вес в килограммах: ")
-	fmt.Scan(&userKg)
-	return userHeight, userKg
-}
-
-func checkRepeatCalculation() bool {
-	var userChoise string
-	fmt.Println(("Вы хотите сделать еще расчёт (y/n): "))
-	fmt.Scan(&userChoise)
-	if userChoise == "y" || userChoise == "Y" {
-		return true
-	}
-	return false
+	return sum
 }
