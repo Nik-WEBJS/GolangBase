e0e3ccfb276e33a3e488c973cafffa71bc4b4209
Nikita Fokin <nikita.fokin0123@gmail.com>
2025-05-24 00:18:38 +0500

    pointers

--- main.go
+++ main.go
@@ -3,70 +3,100 @@ package main
 import (
 	"errors"
 	"fmt"
-	"math"
+	"math/rand/v2"
+	"net/url"
+	"time"
 )
+type account struct{
+	login string
+	password string
+	url string
+}
+
+type accountWithTimestamp struct{
+	createdTime time.Time
+	updatedTime time.Time
+	account
+}
+
+func newAccountWithTimestamp(login, password, urlString string) (*accountWithTimestamp, error){
+	if login == "" {
+		return nil, errors.New("INVALID_LOGIN")
+	}
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("INVALID_URL")
+	}
+	newAcc := &accountWithTimestamp{
+		createdTime: time.Now(),
+		updatedTime: time.Now(),
+		account: account{
+			login: login,
+			password: password,
+			url: urlString,
+		},
+		
+	}
+
+	if password == "" {
+		newAcc.generatePassword(10)
+	}
+	return newAcc, nil 
+}
+
+// func newAccount(login, password, urlString string) (*account, error){
+// 	if login == "" {
+// 		return nil, errors.New("INVALID_LOGIN")
+// 	}
+// 	_, err := url.ParseRequestURI(urlString)
+// 	if err != nil {
+// 		return nil, errors.New("INVALID_URL")
+// 	}
+// 	newAcc := &account{
+// 		login: login,
+// 		password: password,
+// 		url: urlString,
+// 	}
+
+// 	if password == "" {
+// 		newAcc.generatePassword(10)
+// 	}
+// 	return newAcc, nil 
+// }
+
+func (acc *account) outputPass(){
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *account) generatePassword(n int) {
+	res := make([]rune, n)
+
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(len(letterRunes))]
+	} 
+
+	acc.password = string(res)
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 func main() { 
-	for{
-		fmt.Println("Калькулятор индекса массы тела")
-		userHeight, userKg := getUserInput()
-		IMT, err := calculateIMT(userHeight, userKg)
-		if err != nil {
-			fmt.Println("Ошибка, неверно указан вес или рост")
-			continue
-		}
-		outputResult(IMT)
-		isRepeatCalculation := checkRepeatCalculation()
-		if !isRepeatCalculation {
-			break
-		}
+	login:=promtData("Введите логин: ")
+	password:=promtData("Введите пароль: ")
+	url:= promtData("Введите url: ")
+
+	myAccount, err := newAccountWithTimestamp(login, password, url)
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		return
 	}
+	myAccount.outputPass()
 }
 
-func outputResult(IMT float64) {
-	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", IMT)
-	fmt.Println(result)
-		switch {
-	case IMT < 16:
-		fmt.Println("У Вас сильный дефицит массы тела")
-	case IMT < 18.5:
-		fmt.Println("У Вас дефицит массы тела")
-	case IMT < 25:
-		fmt.Println("У Вас нормальная масса тела")
-	case IMT < 30:
-		fmt.Println("У Вас избыточная масса тела")
-	case IMT < 35:
-		fmt.Println("У Вас ожирение первой степени")
-	case  IMT < 40:
-		fmt.Println("У Вас ожирение второй степени")
-	default: fmt.Println("У Вас ожирение третьей степени")
-	}
+func promtData(promt string) string {
+	fmt.Print(promt)
+	var res string
+	fmt.Scanln(&res)
+	return res
 }
 
-func calculateIMT(userHeight, userKg float64) (float64, error) {
-	const IMTPower = 2
-	if userKg <= 0 || userHeight <=0 {
-		return 0, errors.New("ERROR")
-	}
-	IMT := userKg / math.Pow(userHeight / 100, IMTPower)
-	return IMT, nil
-}
-
-func getUserInput() (float64, float64) {
-	var userHeight, userKg float64
-	fmt.Print("Введите ваш рост в сантиметрах: ")
-	fmt.Scan(&userHeight)
-	fmt.Print("Введите ваш вес в килограммах: ")
-	fmt.Scan(&userKg)
-	return userHeight, userKg
-}
-
-func checkRepeatCalculation() bool {
-	var userChoise string
-	fmt.Println(("Вы хотите сделать еще расчёт (y/n): "))
-	fmt.Scan(&userChoise)
-	if userChoise == "y" || userChoise == "Y" {
-		return true
-	}
-	return false
-}
